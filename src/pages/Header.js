1ab2e2a4f5beceb0ad1957e9c63845937016a532
Mori <yuuya15009@gmail.com>
2020-11-17 02:27:41 +0900

    commit

--- src/pages/Header.js
+++ src/pages/Header.js
@@ -3,13 +3,11 @@ import React, { useEffect, useState } from 'react';
 import { IonButtons, IonMenuButton, IonHeader, IonToolbar, IonTitle } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
 import { Redirect, Route } from 'react-router-dom';
-import "./Header.css"
-
 
 const Header = (props) => {
     return (
         <IonHeader>
-            <IonToolbar>
+            <IonToolbar color = "success">
                 <IonTitle>{props.name}</IonTitle>
             </IonToolbar>
         </IonHeader>
