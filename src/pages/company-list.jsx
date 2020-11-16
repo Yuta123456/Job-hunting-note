a6af1ac2cca723f4e5f9b593065322c9034a62fc
Mori <yuuya15009@gmail.com>
2020-11-17 02:26:47 +0900

    commit

--- src/pages/company-list.jsx
+++ src/pages/company-list.jsx
@@ -14,30 +14,69 @@ import {
 } from "@ionic/react";
 import { star } from 'ionicons/icons';
 import ExploreContainer from "../components/ExploreContainer";
-const companyData = [ {name:"三菱UFJ", value:3.5},
-                      {name:"日本大学", value:0.8},
-                      {name:"エイチアイ", value:5.0},
-                      {name:"ゆめみ", value:5.0}];
+import Header from "./Header"
+const companyData = [{
+  name: "三菱UFJ",
+  value: 3.5,
+  data: {
+    企業理念: "",
+    福利厚生: "",
+    年収月収: "",
+    昇給制度: ""
+  }
+}, {
+  name: "日本大学",
+  value: 0.8,
+  data: {
+    企業理念: "",
+    福利厚生: "",
+    年収月収: "",
+    昇給制度: ""
+  }
+}, {
+  name: "エイチアイ",
+  value: 5.0,
+  data: {
+    企業理念: "",
+    福利厚生: "",
+    年収月収: "",
+    昇給制度: ""
+  }
+}, {
+  name: "ゆめみ",
+  value: 5.0,
+  data: {
+    企業理念: "",
+    福利厚生: "",
+    年収月収: "",
+    昇給制度: ""
+  }
+}];
+
+const Detail = (props) => {
+  console.log("OK")
+}
 const Tab1 = () => {
+  const handleClick = (data) => {
+    console.log(data)
+    return (
+      <Detail data={data} />
+    )
+  }
   return (
     <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>企業一覧</IonTitle>
-        </IonToolbar>
-      </IonHeader>
+      <Header name="企業一覧" />
       <IonContent fullscreen>
-        
-          {companyData.map( (data) => 
-            <IonCard>
-              <IonCardHeader>
-                <IonCardTitle>{data.name}</IonCardTitle>
-              </IonCardHeader>
-              <IonCardContent>
-                <IonIcon icon={star}>aiuro</IonIcon> {data.value}
-              </IonCardContent>
-            </IonCard>
-          )}
+        {companyData.map((data) =>
+          <IonCard onClick= {(() => <Detail companyName = {data.neme} data = {data.data} />)}>
+            <IonCardHeader>
+              <IonCardTitle>{data.name}</IonCardTitle>
+            </IonCardHeader>
+            <IonCardContent>
+              <IonIcon icon={star}>aiuro</IonIcon> {data.value}
+            </IonCardContent>
+          </IonCard>
+        )}
       </IonContent>
     </IonPage>
   );
