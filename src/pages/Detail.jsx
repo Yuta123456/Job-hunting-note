0e2bf122ce5bc3a048202503999270902adc4334
Mori <yuuya15009@gmail.com>
2020-11-18 02:42:44 +0900

    commit

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,36 +1,31 @@
 import React, { useState } from "react";
 import {
     IonContent,
-    IonHeader,
     IonPage,
     IonTitle,
-    IonToolbar,
     IonCard,
     IonCardHeader,
     IonCardTitle,
-    IonCardSubtitle,
-    IonButton,
-    IonItem,
-    IonLabel,
-    IonInput,
     IonIcon,
-    IonText,
-    IonRange,
-    IonCardContent
+    IonCardContent,
+    IonActionSheet,
 } from "@ionic/react";
 import Header from '../pages/Header';
 import Footer from '../pages/Footer';
-import { starOutline, ellipsisHorizontal, star } from "ionicons/icons";
+import { star, trash, close, create, navigate } from "ionicons/icons";
+import {useHistory} from "react-router-dom"
+
 const Detail = (props) => {
     const name = props.match.params.name.substr(1)
     const data = JSON.parse(localStorage.companyData)
+    const [showAction, setShowAction] = useState(false)
+    let history = useHistory()
     return (
         <IonPage>
-            <Header name={"企業情報"} />
+            <Header name={"企業情報"} click={setShowAction} flag={true} />
             <IonContent fullscreen>
                 <IonTitle>選択した企業 : {name}</IonTitle>
                 {Object.entries(data[name]).map(value => {
-                    console.log(value)
                     return (
                         <IonCard>
                             <IonCardHeader>
@@ -45,6 +40,31 @@ const Detail = (props) => {
                         </IonCard>
                     )
                 })}
+                <IonActionSheet
+                    isOpen={showAction}
+                    onDidDismiss={() => setShowAction(false)}
+                    header="変更・削除"
+                    buttons={[{
+                        text: "削除",
+                        role: "destructive",
+                        icon: trash,
+                        handler:() => {
+                            history.push("/tab1")
+                            delete data[name]
+                            //localStorage.companyData = JSON.stringify(data)
+                        }
+                    }, {
+                        text: "変更",
+                        icon: create,
+                        handler: () => {
+                            history.push("edit/:")
+                        }
+                    }, {
+                        text: "閉じる",
+                        role: "cancel",
+                        icon: close
+                    }]}
+                />
             </IonContent>
             <Footer />
         </IonPage>
