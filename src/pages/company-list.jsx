85642c4c93c423496c5e6c74e31a41b54b1cec0f
Mori <yuuya15009@gmail.com>
2020-11-17 14:29:12 +0900

    commit

--- src/pages/company-list.jsx
+++ src/pages/company-list.jsx
@@ -1,4 +1,7 @@
+
 import React from "react";
+import Header from './Header';
+import Footer from './Footer';
 import {
   IonContent,
   IonHeader,
@@ -14,72 +17,33 @@ import {
 } from "@ionic/react";
 import { star } from 'ionicons/icons';
 import ExploreContainer from "../components/ExploreContainer";
-import Header from "./Header"
-const companyData = [{
-  name: "三菱UFJ",
-  value: 3.5,
-  data: {
-    企業理念: "",
-    福利厚生: "",
-    年収月収: "",
-    昇給制度: ""
-  }
-}, {
-  name: "日本大学",
-  value: 0.8,
-  data: {
-    企業理念: "",
-    福利厚生: "",
-    年収月収: "",
-    昇給制度: ""
-  }
-}, {
-  name: "エイチアイ",
-  value: 5.0,
-  data: {
-    企業理念: "",
-    福利厚生: "",
-    年収月収: "",
-    昇給制度: ""
-  }
-}, {
-  name: "ゆめみ",
-  value: 5.0,
-  data: {
-    企業理念: "",
-    福利厚生: "",
-    年収月収: "",
-    昇給制度: ""
-  }
-}];
-
-const Detail = (props) => {
-  console.log("OK")
-}
-const Tab1 = () => {
-  const handleClick = (data) => {
-    console.log(data)
-    return (
-      <Detail data={data} />
-    )
-  }
+import { isConstructorDeclaration } from "typescript";
+const Tab1 = (props) => {
+  //localStorage.clear()
+  const companyData = props.data
+  console.log(companyData)
   return (
     <IonPage>
-      <Header name="企業一覧" />
+      <Header name={"企業一覧"} />
       <IonContent fullscreen>
-        {companyData.map((data) =>
-          <IonCard onClick= {(() => <Detail companyName = {data.neme} data = {data.data} />)}>
-            <IonCardHeader>
-              <IonCardTitle>{data.name}</IonCardTitle>
-            </IonCardHeader>
-            <IonCardContent>
-              <IonIcon icon={star}>aiuro</IonIcon> {data.value}
-            </IonCardContent>
-          </IonCard>
-        )}
+        {Object.entries(companyData).map((data) => {
+          {console.log(data)}
+          return (
+            <IonCard>
+              <IonCardHeader>
+                <IonCardTitle>{data[0]}</IonCardTitle>
+              </IonCardHeader>
+              <IonCardContent>
+                <IonIcon icon={star}>aiuro</IonIcon> {data[1].value}
+              </IonCardContent>
+            </IonCard>
+          )
+        })}
       </IonContent>
+      <Footer />
     </IonPage>
   );
 };
 
 export default Tab1;
+
