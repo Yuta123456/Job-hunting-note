68dce790df39673e9e2825fbd25899493477b64d
Mori <yuuya15009@gmail.com>
2020-11-19 02:57:38 +0900

    commit

--- src/pages/company-list.jsx
+++ src/pages/company-list.jsx
@@ -1,5 +1,5 @@
 
-import React from "react";
+import React, { useState} from "react";
 import Header from './Header';
 import Footer from './Footer';
 import {
@@ -19,19 +19,32 @@ const Tab1 = (props) => {
   useIonViewWillEnter(()=>{
     props.setCompanyData(JSON.parse(localStorage.getItem("companyData")));
   });
+  //const [mean, setMean] = useState([])
+  const mean = []
+  Object.values(companyData).map(value => {
+    let total = 0
+    let cnt = 0
+    Object.values(value).map(value => {
+      cnt += 1
+      total += value[1]
+    })
+    mean.push(total / cnt)
+  })
+  console.log(mean)
+  let i = -1
   return (
     <IonPage>
       <Header name={"企業一覧"} />
       <IonContent fullscreen>
         {Object.keys(companyData).map(key => {
+          i += 1
           return (
-            
             <IonCard button routerLink={"/detail/:"+ key}>
               <IonCardHeader>
                 <IonCardTitle>{key}</IonCardTitle>
               </IonCardHeader>
               <IonCardContent>
-                <IonIcon icon={star} color = "warning">aiuro</IonIcon> {}
+                <IonIcon icon={star} color = "warning">aiuro</IonIcon> {mean[i]}
               </IonCardContent>
             </IonCard>
           )
