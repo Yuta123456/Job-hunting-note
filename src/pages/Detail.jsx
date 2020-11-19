169f797404fcbcc0ec38a712a8056d3d4983ba2b
Mori <yuuya15009@gmail.com>
2020-11-20 03:22:49 +0900

    commit

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -12,7 +12,7 @@ import {
 } from "@ionic/react";
 import Header from '../pages/Header';
 import Footer from '../pages/Footer';
-import { star, trash, close, create, search, pencil} from "ionicons/icons";
+import { star, trash, close, create, pencil} from "ionicons/icons";
 import { useHistory } from "react-router-dom"
 import "./Detail.css"
 
